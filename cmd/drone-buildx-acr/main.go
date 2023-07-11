fd20e9a980ef71f0df1e9a9019c6ec5f54b78149
Rutvij Mehta <rutvij.mehta@harness.io>
2023-07-11 13:05:17 -0700

    Docker buildx support

---
+++ cmd/drone-buildx-acr/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+
+	docker "github.com/drone-plugins/drone-buildx"
+)
+
+func main() {
+	// Load env-file if it exists first
+	if env := os.Getenv("PLUGIN_ENV_FILE"); env != "" {
+		godotenv.Load(env)
+	}
+
+	var (
+		repo     = getenv("PLUGIN_REPO")
+		registry = getenv("PLUGIN_REGISTRY")
+		username = getenv("SERVICE_PRINCIPAL_CLIENT_ID")
+		password = getenv("SERVICE_PRINCIPAL_CLIENT_SECRET")
+	)
+
+	// default registry value
+	if registry == "" {
+		registry = "azurecr.io"
+	}
+
+	// must use the fully qualified repo name. If the
+	// repo name does not have the registry prefix we
+	// should prepend.
+	if !strings.HasPrefix(repo, registry) {
+		repo = fmt.Sprintf("%s/%s", registry, repo)
+	}
+
+	os.Setenv("PLUGIN_REPO", repo)
+	os.Setenv("PLUGIN_REGISTRY", registry)
+	os.Setenv("DOCKER_USERNAME", username)
+	os.Setenv("DOCKER_PASSWORD", password)
+	os.Setenv("PLUGIN_REGISTRY_TYPE", "ACR")
+
+	// invoke the base docker plugin binary
+	docker.Run()
+}
+
+func getenv(key ...string) (s string) {
+	for _, k := range key {
+		s = os.Getenv(k)
+		if s != "" {
+			return
+		}
+	}
+	return
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/drone-plugins/drone-buildx-acr
+
+go 1.19
+
+require (
+	github.com/drone-plugins/drone-buildx v1.0.2-0.20230512061850-99d0da95877e
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/drone/drone-go v1.7.1 // indirect
+	github.com/inhies/go-bytesize v0.0.0-20210819104631-275770b98743 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/urfave/cli v1.22.2 // indirect
+	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
+)
